60fd5361fd4eff666eda66e741f235533ba00e5f
donnelly-r <ryan.donnelly19201@gmail.com>
2023-03-01 16:09:02 +0000

    created webpage for first flag

--- website.py
+++ website.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return "<h1>landing page</h1>"
 
+@app.route("/flag1")
+def home():
+    return "<h1>flag 1</h1>"
+
 if __name__ == "__main__":
     app.run()
 
