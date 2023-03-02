ebc65e8361385cb8ba30c8e7a47426789fd553e5
donnelly-r <ryan.donnelly19201@gmail.com>
2023-03-02 14:12:15 +0000

    exported inline html to template files

--- views.py
+++ views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "<h1>landing page</h1>"
+    return render_template("index.html")
 
 @views.route("/flag1")
 def flag1():
-    return "<h1>flag 1</h1>"
+    return render_template("flag1.html")
