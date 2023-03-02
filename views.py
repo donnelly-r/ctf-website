5b0a2457b375b15130f3120c29c9a25aea9b883e
donnelly-r <ryan.donnelly19201@gmail.com>
2023-03-02 16:31:22 +0000

    added css styling from https://github.com/pure-css/pure/tree/master/site/static/layouts/side-menu

--- views.py
+++ views.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
+@views.route("/index")
 def home():
     return render_template("index.html")
 
 @views.route("/flag1")
 def flag1():
     return render_template("flag1.html")
+
+@views.route("/")
+def reroute():
+    return redirect(url_for("views.home"))
