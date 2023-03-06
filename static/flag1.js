0630d38c4ae85020def4a43fc862b3bc41aef15a
donnelly-r <ryan.donnelly19201@gmail.com>
2023-03-06 18:40:07 +0000

    added functionality such that hint 2 and 3 are inaccessible before viewing hint 1

--- static/flag1.js
+++ static/flag1.js
@@ -1,10 +1,30 @@
-function showHint1(){
-    alert('Showing hint 1')
+var hintMessage1 = document.getElementById("hint1text");
+var hintMessage2 = document.getElementById("hint2text");
+var hintMessage3 = document.getElementById("hint3text");
+
+var hintBtn1 = document.getElementById("hint1button");
+var hintBtn2 = document.getElementById("hint2button");
+var hintBtn3 = document.getElementById("hint3button");
+
+//var userAnswer = document.getElementById("flagInput");
+
+hintBtn2.disabled = true;
+hintBtn3.disabled = true;
+
+function showHint1() {
+    hintMessage1.style.display = "block";
+    hintBtn2.disabled = false;
 }
-function showHint2(){
-    alert('Showing hint 2')
+function showHint2() {
+    hintMessage2.style.display = "block";
+    hintBtn3.disabled = false;
 }
-function showHint3(){
-    alert('Showing hint 3')
+function showHint3() {
+    hintMessage3.style.display = "block";
 }
 
+// function checkAnswer(){
+//     if(userAnswer.toLowerCase() == 'nkw2' || userAnswer.toLowerCase() == 'flag{nkw2}'){
+
+//     }
+// }
